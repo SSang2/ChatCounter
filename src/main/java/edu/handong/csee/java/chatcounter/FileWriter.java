ef7f7a2dd628972340b299f7a8cd4b3e7b90278b
leesanghyun499 <37921865+leesanghyun499@users.noreply.github.com>
2018-06-18 21:46:31 +0900

    Update FileWriter.java

--- src/main/java/edu/handong/csee/java/chatcounter/FileWriter.java
+++ src/main/java/edu/handong/csee/java/chatcounter/FileWriter.java
@@ -2,9 +2,6 @@ package edu.handong.csee.java.chatcounter;
 
 import java.io.PrintWriter;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map.Entry;
 /**
@@ -17,19 +14,8 @@ public class FileWriter {
 
 	public void getFile(HashMap<String,Integer> nameAndMessage) {
 
-		ArrayList<String> sortAscen = new ArrayList<String>();
-		sortAscen.addAll(nameAndMessage.keySet());
 		String fileName = "outputResult.csv";
 		PrintWriter output = null;
-		Collections.sort(sortAscen, new Comparator<Object>() {
-
-			public int compare(Object obj1, Object obj2) {
-				Object v1 = nameAndMessage.get(obj1);
-				Object v2 = nameAndMessage.get(obj2);
-				Comparable<Object> comparable = (Comparable<Object>) v1;
-				return comparable.compareTo(v2);
-			}	
-		});
 
 		try {
 			output = new PrintWriter(fileName);
@@ -39,15 +25,10 @@ public class FileWriter {
 				System.out.println(in.getValue());
 			}
 		} catch(FileNotFoundException e) {
-			System.out.println("Error occured to open " + fileName);
+			System.out.println("Error occured" + fileName);
 			System.exit(0);
 		}
 		output.println("ID,count");
-		
-		for(int i=0; i<sortAscen.size(); i++) {
-			output.println(","+nameAndMessage.get(sortAscen.get(i)));
-		}
-	
 		output.close();
 	}
 } 
